f1d15d42115de0189f7ffb4a5b15918a33735066
pciruelos <pciruelos@yahoo.com>
2025-08-01 12:46:59 -0300

    fix:  using top-level await

--- src/main.js
+++ src/main.js
@@ -12,7 +12,8 @@ app.use(pinia)
 app.use(router)
 
 // Inicializar autenticación antes de montar la app
-const authStore = useAuthStore()
-await authStore.initAuth()
-
-app.mount('#app')
+// usariamos await pero como no esta soportado usar await fuera de async en el top level simplement usamos then
+const authStore = useAuthStore();
+authStore.initAuth().then(() => {
+  app.mount('#app');
+});
